c879caa7c6d2f2ca103164b4adba99e24db65542
Roman Berger <romanberger001@gmail.com>
2025-03-05 16:32:25 +0000

    flipping forward and backward in driver

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,13 @@ impl<'d, PWM: PwmPeripheral> Motor<'d, PWM> {
     }
 
     pub fn forward(&mut self) {
-        self.mot_p1.set_timestamp(70);
-        self.mot_p2.set_timestamp(0);
+        self.mot_p1.set_timestamp(0);
+        self.mot_p2.set_timestamp(75);
     }
 
     pub fn backwards(&mut self) {
-        self.mot_p1.set_timestamp(0);
-        self.mot_p2.set_timestamp(70);
+        self.mot_p1.set_timestamp(75);
+        self.mot_p2.set_timestamp(0);
     }
 
     pub fn coast(&mut self) {
