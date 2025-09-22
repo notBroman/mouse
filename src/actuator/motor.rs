7ea672882799fe8d5e2f614820c3f561df6c6ae6
Roman Berger <romanberger001@gmail.com>
2025-09-22 11:48:31 +0200

    reorganise into and group by functionality

--- src/actuator/motor.rs
+++ src/actuator/motor.rs
@@ -1,35 +1,9 @@
 #![no_std]
 
 use esp_backtrace as _;
-use esp_hal::gpio::{Input, InputPin, Level, Output, OutputPin, Pull};
+use esp_hal::gpio::{Input, InputPin, Pull};
 use esp_hal::mcpwm::*;
 use esp_hal::peripheral::Peripheral;
-
-pub struct IRSensor<'d> {
-    trigger: Output<'d>,
-    left_side: Input<'d>,
-    left_front: Input<'d>,
-    right_side: Input<'d>,
-    right_front: Input<'d>,
-}
-
-impl<'d> IRSensor<'d> {
-    pub fn new(
-        mut trig_pin: impl Peripheral<P = impl OutputPin> + 'd,
-        mut lf_pin: impl Peripheral<P = impl InputPin> + 'd,
-        mut ls_pin: impl Peripheral<P = impl InputPin> + 'd,
-        mut rf_pin: impl Peripheral<P = impl InputPin> + 'd,
-        mut rs_pin: impl Peripheral<P = impl InputPin> + 'd,
-    ) -> Self {
-        Self {
-            trigger: Output::new(trig_pin, Level::Low),
-            left_side: Input::new(ls_pin, Pull::Down),
-            left_front: Input::new(lf_pin, Pull::Down),
-            right_side: Input::new(rs_pin, Pull::Down),
-            right_front: Input::new(rf_pin, Pull::Down),
-        }
-    }
-}
 
 pub struct Motor<'d, PWM> {
     // pin 1 & 2 for the motor
@@ -79,13 +53,4 @@ impl<'d, PWM: PwmPeripheral> Motor<'d, PWM> {
         self.mot_p1.set_timestamp(0);
         self.mot_p2.set_timestamp(0);
     }
-}
-
-pub struct Mouse<'d, PWM> {
-    right_mot: Motor<'d, PWM>,
-    left_mot: Motor<'d, PWM>,
-}
-
-impl<'d, PWM: PwmPeripheral> Mouse<'d, PWM> {
-    //pub fn new() -> Self {}
 }
